289f1ff2ec483fe6705077b224bd631b3488964e
techySPHINX <shimon123universe@gmail.com>
2024-03-12 17:40:53 +0530

    Api response commit

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,23 @@
+const asyncHandler = (requestHandler) => {
+   (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).
+        catch((err) => next(err))
+   }
+}
+//this above mentioned code is production grade code
+export {asyncHandler}
+
+
+// const asyncHandler = (fn) => async (req,res,next) => {
+//     try{
+//         await fn(req, res, next)
+//     }catch(error){
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message
+//         })
+//     }
+// }
+
+
+//stadardized code
