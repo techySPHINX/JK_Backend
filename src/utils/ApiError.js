17d3c412d81e589809ea5fa05dbb8c705ba517d7
techySPHINX <shimon123universe@gmail.com>
2024-03-15 01:46:19 +0530

    Modified ApiError.js

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,9 +1,9 @@
 class ApiError extends Error {
     constructor(
         statusCode,
-        message: "Something went wrong",
+        message: "wrong",
         error = [],
-        statck = ""
+        stack = ""
     ){
         super(message)
         this.statusCode = statusCode
@@ -12,8 +12,8 @@ class ApiError extends Error {
         this.success = false;
         this.errors = errors
 
-        if (statck) {
-            this.stack = statck
+        if (stack) {
+            this.stack = stack
         }else{
             Error.captureStackTrace(this, this.constructor)
         }
